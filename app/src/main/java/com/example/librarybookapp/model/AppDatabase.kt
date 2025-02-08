a3c43c343031e3b51fa0c83fdbdb2a925de18ab1
cWalsh-developer <connor_w_98@hotmail.com>
2025-02-08 00:54:05 +0000

    Finalising Database

--- app/src/main/java/com/example/librarybookapp/model/AppDatabase.kt
+++ app/src/main/java/com/example/librarybookapp/model/AppDatabase.kt
@@ -1,7 +1,8 @@
 package com.example.librarybookapp.model
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [Book::class], version = 1)
-abstract class AppDatabase {
+abstract class AppDatabase: RoomDatabase() {
 }
---
+++ app/src/main/java/com/example/librarybookapp/model/DatabaseInstance.kt
@@ -0,0 +1,19 @@
+package com.example.librarybookapp.model
+
+import android.content.Context
+import androidx.room.Room
+import com.example.librarybookapp.model.AppDatabase
+
+object DatabaseInstance {
+    private var INSTANCE: AppDatabase? = null
+
+    fun getDatabase(context: Context): AppDatabase
+    {
+        if(INSTANCE == null)
+        {
+            INSTANCE = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "book-database").build()
+
+        }
+        return INSTANCE!!
+    }
+}
